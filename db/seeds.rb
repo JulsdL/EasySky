f7e678c158bf5006f9e29f6467f29fa8595175ed
Julien de Lambilly <julien.delambilly@gmail.com>
2023-03-07 17:27:13 +0100

    seed json file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create a seed for celestial_bodies from the JSON file catalogue-de-messier.json
+
+require "json"
+require "open-uri"
+
+# Clean the database
+puts "Cleaning database..."
+CelestialBody.destroy_all
+
+file = File.read("db/catalogue-de-messier.json")
+data = JSON.parse(file)
+
+data.each do |celestial_body|
+  messier = CelestialBody.create(
+    name: celestial_body["messier"],
+    description: celestial_body["french_name_nom_français"],
+    ra: celestial_body["ra"],
+    dec: celestial_body["dec"]
+  )
+  # Attach a photo to the celestial body from the url provided in the JSON file
+  file = URI.open(celestial_body["image"])
+  messier.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+  messier.save
+
+  puts "Created #{messier.name}"
+end
