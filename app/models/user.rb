168d6020a23d89dba1b616dd54546d2caf5d64f2
romain isoz <isozromain@gmail.com>
2023-03-07 14:25:35 +0100

    commit pour modification du nom de ma branche

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
