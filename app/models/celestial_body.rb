eb900d2ba22a57bc82f7b07f69c98c8675cc09c8
Faustine Fourcade <faustine.fourcade@hotmail.fr>
2023-03-07 17:06:28 +0100

    ajour espace

--- app/models/celestial_body.rb
+++ app/models/celestial_body.rb
@@ -3,7 +3,7 @@ class CelestialBody < ApplicationRecord
   has_one_attached :photo
 
   validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 10}
+  validates :description, presence: true, length: { minimum: 10 }
   validates :RA, presence: true
   validates :DEC, presence: true
 end
