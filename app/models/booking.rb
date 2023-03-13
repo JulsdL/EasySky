72a3776d6ef74440d4ceaf939d94f50f985bcb9f
Julien de Lambilly <julien.delambilly@gmail.com>
2023-03-13 09:52:10 +0100

    petites corrections

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -17,14 +17,14 @@ class Booking < ApplicationRecord
   # create a new observation planning for each new booking
   def generate_observation_planning
     planning = ObservationPlanning.new
-    # add 8 celestial bodies to the observation planning, wich are the 8 first celestial bodies corresponding
-    # to the ones returned by the visible_objects method
     # add the booking user_id to the observation planning
     planning.user_id = user_id
     # the booking date is the observation planning date and start time is 20:00 and end time is 22:00
     planning.start_time = date + 20.hours
     planning.end_time = date + 22.hours
     planning.name = "Plan d'observation du #{date.strftime("%d.%m.%y")}"
+    # add 8 celestial bodies to the observation planning, wich are the 8 first celestial bodies corresponding
+    # to the ones returned by the visible_objects method
     planning.celestial_bodies = planning.visible_objects.first(8).map { |object| CelestialBody.find_by!(name: object["messier"]) }
     planning.save
   end
