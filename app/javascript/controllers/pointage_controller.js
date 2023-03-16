8144410a009fd2d412eb7a23ec2acaba59367f27
Julien de Lambilly <julien.delambilly@gmail.com>
2023-03-16 10:16:53 +0100

    sweetalert pointage et transmission fonctionnent

--- app/javascript/controllers/pointage_controller.js
+++ app/javascript/controllers/pointage_controller.js
@@ -1,11 +1,9 @@
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus";
 import Swal from "sweetalert2";
 
-export default class PointageController extends Controller {
-  // ...
-
+export default class extends Controller {
   pointTelescope(event) {
-    const celestialBodyName = event.currentTarget.dataset.celestialBodyName;
+    const celestialBodyName = event.currentTarget.getAttribute('data-pointage-celestial-body-name');
 
     Swal.fire({
       title: `Pointer le télescope vers ${celestialBodyName}?`,
@@ -15,7 +13,21 @@ export default class PointageController extends Controller {
       cancelButtonText: "Annuler",
     }).then((result) => {
       if (result.isConfirmed) {
-        // Vous pouvez ajouter ici des actions à effectuer si l'utilisateur confirme
+        // Si l'utilisateur confirme, affichez un message "Pointage en cours..."
+        Swal.fire({
+          title: `Transmission au télescope en cours...`,
+          iconHtml: '<i class="fa-solid fa-satellite-dish"></i>',
+          showConfirmButton: false,
+          allowOutsideClick: true, // Autoriser la fermeture en cliquant en dehors de la fenêtre
+          allowEscapeKey: false,
+          allowEnterKey: false,
+          timerProgressBar: true,
+          timer: 4000, // Le message disparaîtra après 4 secondes (4000 ms)
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("Le pointage a été effectué.");
+          }
+        });
       }
     });
   }
